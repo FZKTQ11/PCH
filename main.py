f686e1f790928253a852b5e5d0b8458915cfe4f6
FZKTQ11 <nikolaev.eg@km-spb.com>
2025-04-29 16:00:28 +0300

    main.py

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
 import unittest
-class TestGetMaxCount(unittest.TestCase):
-def test_empty_array(self):
-arr = []
-expected = 0
-result = GetMaxCount(arr)
-self.assertEqual(result, expected)
-def test_single_max_value(self):
-arr = [1, 2, 3, 4, 5, 5]
-expected = 2
-result = GetMaxCount(arr)
-self.assertEqual(result, expected)
+from iarray import count_occurrences_of_max_value
 
-def test_multiple_max_values(self):
-arr = [1, 5, 3, 5, 4, 5]
-expected = 3
-result = GetMaxCount(arr)
-self.assertEqual(result, expected)
-if __name__ == &#39;__main__&#39;:
-unittest.main()
+class TestGetMaxCount(unittest.TestCase):
+    def test_empty_array(self):
+        arr = []
+        expected = 0
+        result = count_occurrences_of_max_value(arr)
+        self.assertEqual(result, expected)
+
+    def test_single_max_value(self):
+        arr = [1, 2, 3, 4, 5, 5]
+        expected = 2
+        result = count_occurrences_of_max_value(arr)
+        self.assertEqual(result, expected)
+
+    def test_multiple_max_values(self):
+        arr = [1, 5, 3, 5, 4, 5]
+        expected = 3
+        result = count_occurrences_of_max_value(arr)
+        self.assertEqual(result, expected)
+
+if __name__ == '__main__':
+    unittest.main()
+  
