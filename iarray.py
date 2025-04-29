a515ff9b5f2c187e957efc21969f1d831c69f60a
FZKTQ11 <nikolaev.eg@km-spb.com>
2025-04-29 16:01:12 +0300

    Update iarray.py

--- iarray.py
+++ iarray.py
@@ -1,14 +1,5 @@
-def count_max_elements(arr):
-max_elem = max(arr) # находим максимальный элемент в
-массиве
-count = 0 # инициализируем счетчик
-for elem in arr: # проходим по всем элементам массива
-if elem == max_elem: # если текущий элемент равен
-максимальному
-count += 1 # увеличиваем счетчик
-return count # возвращаем количество максимальных
-элементов
-# пример использования функции
-arr = [1, 3, 5, 7, 5, 2, 5, 8, 5, 4] # ПОДСТАВИТЬ СВОИ ЗНАЧЕНИЯ
-count = count_max_elements(arr)
-print(&quot;Количество максимальных элементов в массиве:&quot;, count)
+def count_occurrences_of_max_value(arr):
+    if not arr:
+        return 0
+    max_elem = max(arr)
+    return arr.count(max_elem)
